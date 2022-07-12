d1edd3e1805494822d40dfad6ce801ac1a2c5387
IshikoroKoishi <koishi1@hotmail.co.jp>
2022-07-13 06:41:44 +0900

    Path短縮

--- .config/powershell/profile.ps1
+++ .config/powershell/profile.ps1
@@ -1,8 +1,23 @@
 function prompt {
+    $Sep = "/"
     $CurrentDir = (Convert-Path .)
     if ($CurrentDir.Contains($HOME)) {
         $CurrentDir = $CurrentDir.Replace($HOME, "~")
     }
+    $PArry = $CurrentDir.Split($Sep)
+    $Len = $PArry.Count - 1
+    $Idx = 0
+    $NArry = @()
+    foreach ($P in $PArry) {
+        if ($Idx -lt $Len) {
+            $NArry += $P.Substring(0, 1)
+        } else {
+            $NArry += $P
+        }
+        $Idx = $Idx + 1
+    }
+    $CurrentDir = [String]::Join($Sep, $NArryls
+    )
 
     $RightSidePrompt = ("[" + (Get-Date -Format 'yyyy/MM/dd(ddd)HH:mm:ss') + "]")
     $StartPosX = $Host.UI.RawUI.WindowSize.Width - $RightSidePrompt.Length
