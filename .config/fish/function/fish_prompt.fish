7af1ff8773c0245ad19a901bd0b19db4b5700a70
IshikoroKoishi <koishi1@hotmail.co.jp>
2022-07-15 22:40:37 +0900

    右プロンプト削除

--- .config/fish/function/fish_prompt.fish
+++ .config/fish/function/fish_prompt.fish
@@ -7,14 +7,9 @@ function fish_prompt --description "Main prompt"
     set -l statusb_color (set_color --bold $fish_color_status)
     set -l pipestatus_string (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
 
-    printf '%s%s@%s%s %s%s %s%s%s%s \n$ '  \
+    printf '%s[%s] %s%s@%s%s %s%s %s%s%s%s \n$ '  \
+        (set_color $fish_color_host) (date '+%Y/%m/%d(%a)%H:%M:%S') \
         (set_color $fish_color_user) $USER \
         (set_color $fish_color_host) (prompt_hostname) \
         (set_color $fish_color_cwd) (prompt_pwd) $pipestatus_string (set_color normal)
-
-end
-
-
-function fish_right_prompt --description "Right-side prompt"
-    printf '%s[%s]' (set_color $fish_color_host) (date '+%Y/%m/%d(%a)%H:%M:%S')
 end
